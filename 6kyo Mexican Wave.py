1f9ab22bb8565cc3d1ebf1d73f2fd89947dea691
Matvey2009 <matvey2009ua@gmail.com>
2023-07-07 21:42:38 +0300

    Update 6kyo Mexican Wave.py

--- 6kyo Mexican Wave.py
+++ 6kyo Mexican Wave.py
@@ -1,14 +1,9 @@
-people = input()
-t = 0
-arr = []
-for i in range(len(people)):
-	if(people!=" "):
-		f = people[i+t]
-		f.title()
-		s = people[i+t:] + f.upper + people[:i+t+1]
-		arr.append(s)
-	else:
-		t += 1
-print(arr)
-
-#https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/python
+def wave(people):
+    t = 0
+    arr = []
+    for e, i in enumerate(people):
+        if(i!=" "):
+            arr.append(people[:e] + people[e].upper() + people[e+1:])
+        else:
+            t += 1
+    return arr
