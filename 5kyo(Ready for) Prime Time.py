61f0e0fd63dc1c0d4b59463eecafa4d46dfd8171
Matvey2009 <matvey2009ua@gmail.com>
2023-07-12 20:42:08 +0300

    Update 5kyo(Ready for) Prime Time.py

--- 5kyo(Ready for) Prime Time.py
+++ 5kyo(Ready for) Prime Time.py
@@ -1,10 +1,14 @@
-n = int(input())
-arr = {2}
-for i in range(n):
-	for j in range(2, int(i ** 0.5) + 1):
-		if i % j == 0:
-			break
-		else:
-			arr.update(i)
-print(arr)
-
+def prime(n):
+    arr = []
+    if n == 2:
+        arr = [2]
+        return arr
+    for i in range(2, n+1):
+        test = 0
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                test = 1
+                break
+        if test == 0:
+            arr.append(i)
+    return arr
